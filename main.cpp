d38cac268d84c681ce320cdcb6e2231201c2f0ab
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 21:29:04 +1300

    Added source file information.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "In Visual Studio, a C++ program consists of source files (including header files and .cpp files) which are organized in a project which is stored inside a solution" << "\n";
+
     return 0;
 }
