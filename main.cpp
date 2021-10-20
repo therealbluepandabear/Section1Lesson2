4daa30ad9674887a562d14eefd2e9b12f7e51f53
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 21:31:05 +1300

    Added information on how to declare comments in C++.

--- main.cpp
+++ main.cpp
@@ -3,5 +3,12 @@
 int main() {
     std::cout << "In Visual Studio, a C++ program consists of source files (including header files and .cpp files) which are organized in a project which is stored inside a solution" << "\n";
 
+    std::cout << "In C++ you can specify comments in two ways:" << "\n";
+
+    // Like this (single line comment)
+    /*
+     * Or like this (multiline comment)
+     */
+
     return 0;
 }
